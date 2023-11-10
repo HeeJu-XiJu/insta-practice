1149cbd80b026f6bc2ed64fc36150841ce4de649
hhjjoa2817 <hhjjoa2817@naver.com>
2023-11-10 10:46:35 +0900

    like 기능 구현

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Post
 from .form import PostForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -27,3 +28,17 @@ def create(request):
         'form': form,
     }
     return render(request, 'form.html', context)
+
+@login_required
+def likes(request, id):
+    user = request.user
+    post = Post.objects.get(id=id)
+
+    # 이미 좋아요 버튼을 누른 경우
+    if post in user.like_posts.all():
+        post.like_users.remove(user)
+    # 좋아요 버튼을 아직 안 누른 경우
+    else:
+        post.like_users.add(user)
+        # user.like_posts.add(post)
+    return redirect('posts:index')
